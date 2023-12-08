035d31fa6eac1cfb39d91f0474b9e1243839cbab
Omar Lawatey <omarlawatey@gmail.com>
2023-12-08 22:38:59 +0200

    Feat: Add Chakra-ui Library

---
+++ src/theme.js
@@ -0,0 +1,32 @@
+import { extendTheme } from '@chakra-ui/react';
+
+// Global Values
+const globalStyles = {
+  global: {
+    '*': {
+      WebkitTapHighlightColor: 'transparent'
+    },
+    body: {
+      scrollbarGutter: 'stable',
+      minH: '100vh',
+      minW: '100vw',
+      fontSize: '32px',
+      fontWeight: '700'
+    }
+  }
+};
+
+// Base Values
+const colors = {
+  white: 'hsl(0, 0%, 100%)'
+};
+
+export default extendTheme({
+  styles: {
+    ...globalStyles
+  },
+  colors,
+  fonts: {
+    body: '"Spartan", sans-serif'
+  }
+});
